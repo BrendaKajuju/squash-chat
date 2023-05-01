bca7400ee667dfdf36bca89b75b2ebc42b3e3afa
Brwni <brianmm665@gmial.com>
2023-05-01 14:08:01 +0300

    Initial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,23 @@ import { GoogleOutlined, FacebookOutlined} from "@ant-design/icons"
  //creating the login in page for the application//
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div id="login-page">
+       <div id="login-card">
+          <h2>Welcome To Squash-Chat!</h2>
+              
+          <div
+           className="login-button google"
+          >
+            <GoogleOutlined /> Sign In with Google
+          </div>
+
+          <br /> <br />
+
+          <div className="login-button facebook"
+          >
+            <FacebookOutlined /> Sign In with FaceBook
+          </div>
+       </div>
+    </div>
+  );
 }
