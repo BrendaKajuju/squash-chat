f7bd9a177820d42683cb2d23b17e3d7d1425fac8
Brwni <brianmm665@gmial.com>
2023-05-01 13:55:17 +0300

    trial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { GoogleOutlined, FacebookOutlined} from "@ant-design/icons"
-
+ //creating the login in page for the application//
 export const Login = () => {
   return (
     <div>Login</div>
