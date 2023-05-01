4c6eeaa34c0af9f0772b151d3c4a14981deeb2ad
Brenda <brendakajuju777@gmail.com>
2023-05-01 19:35:21 +0300

    add chat component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useHistory, useState, useEffect } from 'react'
 import {BrowserRouter  as Router, Switch, Route } from "react-router-dom"
 import './App.css'
+import { AuthenticationProvider } from './context/AuthenticationContext'
 import Login from "./components/Login"
+import Chat from './components/Chat'
 
 // Tasks;
 //   1. import context component
@@ -12,11 +14,13 @@ function App() {
   return (
  <div>
   <Router>
-    <switch>
+    <AuthenticationProvider>
+      <switch>
       {/* //render either one of the components below */}
+      <Route path="/" component={Chat} />
       <Route path="/" component={Login} />
-    </switch>
-
+      </switch>
+    </AuthenticationProvider>
   </Router>
  </div>
   )
