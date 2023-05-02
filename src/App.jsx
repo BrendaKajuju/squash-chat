8b5af5a1340f45859500873d7acb487c071c6463
Brwni <brianmm665@gmial.com>
2023-05-02 12:51:21 +0300

    Add chat content

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Login from "./components/Login"
 
 // Tasks;
 //   1. import context component
-//   2. import login component
-//   3. import chat component
+
+// import Login from "./components/Login"
+
+// import Chats from "./components/Chats"
 
 function App() {
   return (
