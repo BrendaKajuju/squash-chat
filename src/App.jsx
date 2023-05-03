744cf4c6b8fab124b57a6a49dce01f117f4c3814
Brian Mwangi Maina <118651060+BrwnBoy@users.noreply.github.com>
2023-05-03 12:14:17 +0300

    Revert "Fixed Route"

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,31 @@
-// import { useNavigate, useState, useEffect } from 'react'
-// import {BrowserRouter  as Router, Routes, Route } from "react-router-dom"
-// import './App.css'
-// import { AuthenticationProvider } from './context/AuthenticationContext'
-// // import Login from "./components/Login"
-// // import Chat from './components/Chats'
-
-// // Tasks;
-// //   1. import context component
-
-// // import Login from "./components/Login"
-
-// import Chats from "./components/Chats"
-
-// function App() {
-//   return (
-//  <div>
-//   <Router>
-//     <AuthenticationProvider>
-//       <Routes>
-//       {/* //render either one of the components below */}
-//       <Route path="/" component={Chats} />
-//       <Route path="/" component={Login} />
-//     </Routes>
-//     </AuthenticationProvider>
-//   </Router>
-//  </div>
-//   )
-// }
-
-// export default App;
 import { useNavigate, useState, useEffect } from 'react'
 import {BrowserRouter  as Router, Routes, Route } from "react-router-dom"
 import './App.css'
 import { AuthenticationProvider } from './context/AuthenticationContext'
 import Login from "./components/Login"
+import Chat from './components/Chats'
+
+// Tasks;
+//   1. import context component
+
+// import Login from "./components/Login"
+
 import Chats from "./components/Chats"
 
 function App() {
   return (
-    <div>
-      <Router>
-        <AuthenticationProvider>
-          <Routes>
-            {/* Render either one of the components below */}
-            <Route path="/" element={<Login />} />
-            <Route path="/chats" element={<Chats />} />
-          </Routes>
-        </AuthenticationProvider>
-      </Router>
-    </div>
+ <div>
+  <Router>
+    <AuthenticationProvider>
+      <Routes>
+      {/* //render either one of the components below */}
+      <Route path="/" component={Chats} />
+      <Route path="/" component={Login} />
+    </Routes>
+    </AuthenticationProvider>
+  </Router>
+ </div>
   )
 }
 
 export default App;
-
-// With this configuration, the Login component will be rendered on the root route ("/") and the Chats component will be rendered on the "/chats" route. You can then use useNavigate hook to navigate between the routes in your components.
